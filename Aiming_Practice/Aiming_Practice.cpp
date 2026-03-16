65e1148d5d70e3841320fc919ddea4c85cd6a721
Stanko Madzar <stanko.madzar@outlook.com>
2026-03-16 20:56:37 +0100

    Opened first Render Window in SFML

--- Aiming_Practice/Aiming_Practice.cpp
+++ Aiming_Practice/Aiming_Practice.cpp
@@ -7,5 +7,29 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Window
+    sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "Aiming Practice", sf::Style::Titlebar | sf::Style::Close);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+
+    //Game Loop
+    while (window.isOpen())
+    {
+        //Event polling
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        //Update
+
+        //Render
+        window.clear();     // clear old frame
+        window.draw(shape); // draw shape
+        window.display();   // tell game that the window is done drawing
+    }
+    
+    return 0;
 }
